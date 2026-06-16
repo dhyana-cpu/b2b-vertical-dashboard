5ff853ce395d1defcddf29269975261125186755
adityasanghavi-sys <aditya.sanghavi@snackible.com>
2026-06-17 00:52:22 +0530

    Update sheet.js

--- api/sheet.js
+++ api/sheet.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const url = 'https://script.google.com/macros/s/AKfycbwlZ0Ei--gRGtV3QiKLbZb_zvA23r7_potGuR5qnDSLerHIJYiSi3QJBx0yCoZO7d7jVA/exec';
+  const url = 'https://script.google.com/macros/s/AKfycbwmJTQwYH_8BgIwBR-orMdkq58Wnnx0e2d5aEPnkwV4duxmTS7TEeKauT0raB-nTj6f8A/exec';
   try {
     const r = await fetch(url, { redirect: 'follow' });
     const data = await r.json();
